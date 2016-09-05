68daef4c7ff8f0cf99aec6809705c0cf41a7b175
Ravindra Kumar Singh <ravindra45.singh@gmail.com>
2016-09-05 00:47:53 -0700

    added logger to Main appliction

--- src/main/java/com/infosys/application/AssetApplication.java
+++ src/main/java/com/infosys/application/AssetApplication.java
@@ -1,20 +1,26 @@
 package com.infosys.application;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.PropertySource;
 
+import com.infosys.service.rest.asset.AssetRestController;
+
 @SpringBootApplication
-@ComponentScan( basePackages = { "com.infosys.service.rest.asset" } )
-@PropertySource( "classpath:asset_rest_service.properties" )
+@ComponentScan(basePackages = { "com.infosys.service.rest.asset" })
+@PropertySource("classpath:asset_rest_service.properties")
 public class AssetApplication {
-    public static void main( String[] args ) {
-    	System.out.println("Main Method Called");
-        SpringApplication app = new SpringApplication( AssetApplication.class );
-        app.setBannerMode( Banner.Mode.CONSOLE );
-        app.run( args );
-        System.out.println("Asset Application started");
-    }
+	private static final Logger LOGGER = LoggerFactory.getLogger(AssetRestController.class);
+
+	public static void main(String[] args) {
+		SpringApplication app = new SpringApplication(AssetApplication.class);
+		app.setBannerMode(Banner.Mode.CONSOLE);
+		app.run(args);
+		System.out.println("Asset Application started");
+		LOGGER.info("o;***** Asset Application started *****");
+	}
 }
